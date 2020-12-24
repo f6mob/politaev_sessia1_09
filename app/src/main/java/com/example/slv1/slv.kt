6eb1c628de85bc0b00d8958c88fb2097ea3b3971
user <user@C0910.local>
2020-12-24 14:28:05 +0400

    Initial commit

---
+++ app/src/main/java/com/example/slv1/slv.kt
@@ -0,0 +1,14 @@
+package com.example.slv1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class slv : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slv);
+
+
+    }
+}
